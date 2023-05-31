8a1f83ccdeecd9fe07cea3f0531de0bd92642f89
mitchell <70453897+orbitalquark@users.noreply.github.com>
2023-05-31 19:12:16 -0400

    Reformat source code to use tabs for indentation. Requires some bugfixes to LuaFormatter's handling of tabs.

--- init.lua
+++ init.lua
@@ -56,19 +56,19 @@ M.INDIC_SPELLING = _SCINTILLA.new_indic_number()
 -- Localizations.
 local _L = _L
 if not rawget(_L, 'Spelling') then
-  -- Menu.
-  _L['Spelling'] = 'Spell_ing'
-  _L['Check Spelling...'] = '_Check Spelling...'
-  _L['Mark Misspelled Words'] = '_Mark Misspelled Words'
-  _L['Load Dictionary...'] = '_Load Dictionary...'
-  _L['Select Dictionary'] = 'Select Dictionary'
-  _L['Open User Dictionary'] = '_Open User Dictionary'
-  -- Other.
-  _L['Language not found'] = 'Language not found'
-  _L['No Suggestions'] = 'No Suggestions'
-  _L['Add'] = 'Add'
-  _L['Ignore'] = 'Ignore'
-  _L['No misspelled words.'] = 'No misspelled words.'
+	-- Menu.
+	_L['Spelling'] = 'Spell_ing'
+	_L['Check Spelling...'] = '_Check Spelling...'
+	_L['Mark Misspelled Words'] = '_Mark Misspelled Words'
+	_L['Load Dictionary...'] = '_Load Dictionary...'
+	_L['Select Dictionary'] = 'Select Dictionary'
+	_L['Open User Dictionary'] = '_Open User Dictionary'
+	-- Other.
+	_L['Language not found'] = 'Language not found'
+	_L['No Suggestions'] = 'No Suggestions'
+	_L['Add'] = 'Add'
+	_L['Ignore'] = 'Ignore'
+	_L['No misspelled words.'] = 'No misspelled words.'
 end
 
 local lib = 'spellcheck.spell'
@@ -77,9 +77,9 @@ M.spell = require(lib)
 
 --- Paths to search for Hunspell dictionaries in.
 M.hunspell_paths = {
-  _USERHOME .. '/modules/spellcheck/', '/usr/local/share/hunspell/', '/usr/share/hunspell/',
-  'C:\\Program Files (x86)\\hunspell\\', 'C:\\Program Files\\hunspell\\',
-  _HOME .. '/modules/spellcheck/'
+	_USERHOME .. '/modules/spellcheck/', '/usr/local/share/hunspell/', '/usr/share/hunspell/',
+	'C:\\Program Files (x86)\\hunspell\\', 'C:\\Program Files\\hunspell\\',
+	_HOME .. '/modules/spellcheck/'
 }
 
 --- Table of spellcheckable style names.
@@ -95,23 +95,23 @@ local user_dicts = _USERHOME .. (not WIN32 and '/' or '\\') .. 'dictionaries'
 -- @param lang The hunspell language to load.
 -- @usage spellcheck.load('en_US')
 function M.load(lang)
-  local aff, dic = lang .. '.aff', lang .. '.dic'
-  for _, path in ipairs(M.hunspell_paths) do
-    local aff_path, dic_path = path .. aff, path .. dic
-    if lfs.attributes(aff_path) and lfs.attributes(dic_path) then
-      M.spellchecker = M.spell(aff_path, dic_path)
-      goto lang_found
-    end
-  end
-  error(_L['Language not found'] .. ': ' .. lang)
-  ::lang_found::
-  if lfs.attributes(user_dicts) then
-    for dic in lfs.dir(user_dicts) do
-      if dic:find('^%.%.?$') then goto continue end
-      M.spellchecker:add_dic(user_dicts .. (not WIN32 and '/' or '\\') .. dic)
-      ::continue::
-    end
-  end
+	local aff, dic = lang .. '.aff', lang .. '.dic'
+	for _, path in ipairs(M.hunspell_paths) do
+		local aff_path, dic_path = path .. aff, path .. dic
+		if lfs.attributes(aff_path) and lfs.attributes(dic_path) then
+			M.spellchecker = M.spell(aff_path, dic_path)
+			goto lang_found
+		end
+	end
+	error(_L['Language not found'] .. ': ' .. lang)
+	::lang_found::
+	if lfs.attributes(user_dicts) then
+		for dic in lfs.dir(user_dicts) do
+			if dic:find('^%.%.?$') then goto continue end
+			M.spellchecker:add_dic(user_dicts .. (not WIN32 and '/' or '\\') .. dic)
+			::continue::
+		end
+	end
 end
 M.load((os.getenv('LANG') or ''):match('^[^.@]+') or 'en_US')
 events.connect(events.RESET_BEFORE, function() M.spellchecker = nil end)
@@ -119,68 +119,68 @@ events.connect(events.RESET_BEFORE, function() M.spellchecker = nil end)
 --- Shows suggestions for string *word* at the current position.
 -- @param word The word to show suggestions for.
 local function show_suggestions(word)
-  local encoding = M.spellchecker:get_dic_encoding()
-  local suggestions = M.spellchecker:suggest(word:iconv(encoding, 'UTF-8'))
-  for i = 1, #suggestions do suggestions[i] = suggestions[i]:iconv('UTF-8', encoding) end
-  if #suggestions == 0 then suggestions[1] = string.format('(%s)', _L['No Suggestions']) end
-  suggestions[#suggestions + 1] = string.format('(%s)', _L['Add'])
-  suggestions[#suggestions + 1] = string.format('(%s)', _L['Ignore'])
-  local separator = buffer.auto_c_separator
-  buffer.auto_c_separator = string.byte('\n')
-  buffer:user_list_show(SPELLING_ID, table.concat(suggestions, '\n'))
-  buffer.auto_c_separator = separator
+	local encoding = M.spellchecker:get_dic_encoding()
+	local suggestions = M.spellchecker:suggest(word:iconv(encoding, 'UTF-8'))
+	for i = 1, #suggestions do suggestions[i] = suggestions[i]:iconv('UTF-8', encoding) end
+	if #suggestions == 0 then suggestions[1] = string.format('(%s)', _L['No Suggestions']) end
+	suggestions[#suggestions + 1] = string.format('(%s)', _L['Add'])
+	suggestions[#suggestions + 1] = string.format('(%s)', _L['Ignore'])
+	local separator = buffer.auto_c_separator
+	buffer.auto_c_separator = string.byte('\n')
+	buffer:user_list_show(SPELLING_ID, table.concat(suggestions, '\n'))
+	buffer.auto_c_separator = separator
 end
 -- Either correct the misspelled word, add it to the user's dictionary, or ignore it (based on
 -- the selected item).
 events.connect(events.USER_LIST_SELECTION, function(id, text, position)
-  if id ~= SPELLING_ID then return end
-  local s = buffer:indicator_start(M.INDIC_SPELLING, position)
-  local e = buffer:indicator_end(M.INDIC_SPELLING, position)
-  if not text:find('^%(') then
-    local line = buffer:line_from_position(position)
-    local column = buffer.column[position]
-    buffer:set_target_range(s, e)
-    buffer:replace_target(text)
-    buffer:goto_pos(buffer:find_column(line, column))
-  else
-    local word = buffer:text_range(s, e)
-    if text:find(_L['Add']) then
-      if not lfs.attributes(user_dicts) then lfs.mkdir(user_dicts) end
-      local user_dict = user_dicts .. '/user.dic'
-      local words = {}
-      if lfs.attributes(user_dict) then
-        for word in io.lines(user_dict) do words[#words + 1] = word end
-      end
-      words[1] = #words + 1
-      words[#words + 1] = word
-      io.open(user_dict, 'wb'):write(table.concat(words, '\n')):close()
-    end
-    M.spellchecker:add_word(word:iconv(M.spellchecker:get_dic_encoding(), 'UTF-8'))
-    M.check_spelling() -- clear highlighting for all occurrences
-  end
+	if id ~= SPELLING_ID then return end
+	local s = buffer:indicator_start(M.INDIC_SPELLING, position)
+	local e = buffer:indicator_end(M.INDIC_SPELLING, position)
+	if not text:find('^%(') then
+		local line = buffer:line_from_position(position)
+		local column = buffer.column[position]
+		buffer:set_target_range(s, e)
+		buffer:replace_target(text)
+		buffer:goto_pos(buffer:find_column(line, column))
+	else
+		local word = buffer:text_range(s, e)
+		if text:find(_L['Add']) then
+			if not lfs.attributes(user_dicts) then lfs.mkdir(user_dicts) end
+			local user_dict = user_dicts .. '/user.dic'
+			local words = {}
+			if lfs.attributes(user_dict) then
+				for word in io.lines(user_dict) do words[#words + 1] = word end
+			end
+			words[1] = #words + 1
+			words[#words + 1] = word
+			io.open(user_dict, 'wb'):write(table.concat(words, '\n')):close()
+		end
+		M.spellchecker:add_word(word:iconv(M.spellchecker:get_dic_encoding(), 'UTF-8'))
+		M.check_spelling() -- clear highlighting for all occurrences
+	end
 end)
 
 -- LPeg grammar that matches spellcheckable words.
 local word_patt = {
-  lpeg.Cp() * lpeg.C(lpeg.V('word')) * lpeg.Cp() + lpeg.V('skip') * lpeg.V(1),
-  word_char = lpeg.R('AZ', 'az', '09', '\127\255') + '_',
-  word_part = lpeg.R('az', '\127\255')^1 * -lpeg.V('word_char') +
-    (lpeg.R('AZ') * lpeg.R('az', '\127\255')^0 * -lpeg.V('word_char')) +
-    (lpeg.R('AZ', '\127\255')^1 * -lpeg.V('word_char')),
-  word = lpeg.V('word_part') * (lpeg.S("-'") * lpeg.V('word_part'))^-1 *
-    -(lpeg.V('word_char') + lpeg.S("-'.") * lpeg.V('word_char')),
-  skip = lpeg.V('word_char')^1 * (lpeg.S("-'.") * lpeg.V('word_char')^1)^0 +
-    (1 - lpeg.V('word_char'))^1
+	lpeg.Cp() * lpeg.C(lpeg.V('word')) * lpeg.Cp() + lpeg.V('skip') * lpeg.V(1),
+	word_char = lpeg.R('AZ', 'az', '09', '\127\255') + '_',
+	word_part = lpeg.R('az', '\127\255')^1 * -lpeg.V('word_char') +
+		(lpeg.R('AZ') * lpeg.R('az', '\127\255')^0 * -lpeg.V('word_char')) +
+		(lpeg.R('AZ', '\127\255')^1 * -lpeg.V('word_char')),
+	word = lpeg.V('word_part') * (lpeg.S("-'") * lpeg.V('word_part'))^-1 *
+		-(lpeg.V('word_char') + lpeg.S("-'.") * lpeg.V('word_char')),
+	skip = lpeg.V('word_char')^1 * (lpeg.S("-'.") * lpeg.V('word_char')^1)^0 +
+		(1 - lpeg.V('word_char'))^1
 }
 
 --- Returns a generator that acts like string.gmatch, but for LPeg patterns.
 -- @param pattern LPeg pattern.
 -- @param subject String subject.
 local function lpeg_gmatch(pattern, subject)
-  return function(subject, i)
-    local s, word, e = lpeg.match(pattern, subject, i)
-    if word then return e, s, word end
-  end, subject, 1
+	return function(subject, i)
+		local s, word, e = lpeg.match(pattern, subject, i)
+		if word then return e, s, word end
+	end, subject, 1
 end
 
 --- Checks the buffer for spelling errors, marks misspelled words, and optionally shows
@@ -191,77 +191,77 @@ end
 --   displaying useful statusbar information. This flag is used and set internally, and should
 --   not be set otherwise.
 function M.check_spelling(interactive, wrapped)
-  -- Show suggestions for the misspelled word under the caret if necessary.
-  if interactive and buffer:indicator_all_on_for(buffer.current_pos) & 1 << M.INDIC_SPELLING - 1 > 0 then
-    local s = buffer:indicator_start(M.INDIC_SPELLING, buffer.current_pos)
-    local e = buffer:indicator_end(M.INDIC_SPELLING, buffer.current_pos)
-    show_suggestions(buffer:text_range(s, e))
-    return
-  end
-  -- Clear existing spellcheck indicators.
-  buffer.indicator_current = M.INDIC_SPELLING
-  if not interactive then buffer:indicator_clear_range(1, buffer.length) end
-  -- Iterate over spellcheckable text ranges, checking words in them, and marking misspellings.
-  local spellcheckable_styles = {} -- cache
-  local buffer, style_at = buffer, buffer.style_at
-  local encoding = M.spellchecker:get_dic_encoding()
-  local i = (not interactive or wrapped) and 1 or
-    buffer:word_start_position(buffer.current_pos, false)
-  while i <= buffer.length do
-    -- Ensure at least the next page of text is styled since spellcheckable ranges depend on
-    -- accurate styling.
-    if i > buffer.end_styled then
-      local next_page = buffer:line_from_position(i) + view.lines_on_screen
-      buffer:colorize(buffer.end_styled, buffer.line_end_position[next_page])
-    end
-    local style = style_at[i]
-    if spellcheckable_styles[style] == nil then
-      -- Update the cache.
-      local style_name = buffer:name_of_style(style)
-      spellcheckable_styles[style] = M.spellcheckable_styles[style_name] == true
-    end
-    if spellcheckable_styles[style] then
-      local j = i + 1
-      while j <= buffer.length and style_at[j] == style do j = j + 1 end
-      for e, s, word in lpeg_gmatch(word_patt, buffer:text_range(i, j)) do
-        local ok, encoded_word = pcall(string.iconv, word, encoding, 'UTF-8')
-        if not M.spellchecker:spell(ok and encoded_word or word) then
-          buffer:indicator_fill_range(i + s - 1, e - s)
-          if interactive then
-            buffer:goto_pos(i + s - 1)
-            show_suggestions(word)
-            return
-          end
-        end
-      end
-      i = j
-    else
-      i = i + 1
-    end
-  end
-  if interactive then
-    if not wrapped then
-      M.check_spelling(true, true) -- wrap
-      return
-    end
-    ui.statusbar_text = _L['No misspelled words.']
-  end
+	-- Show suggestions for the misspelled word under the caret if necessary.
+	if interactive and buffer:indicator_all_on_for(buffer.current_pos) & 1 << M.INDIC_SPELLING - 1 > 0 then
+		local s = buffer:indicator_start(M.INDIC_SPELLING, buffer.current_pos)
+		local e = buffer:indicator_end(M.INDIC_SPELLING, buffer.current_pos)
+		show_suggestions(buffer:text_range(s, e))
+		return
+	end
+	-- Clear existing spellcheck indicators.
+	buffer.indicator_current = M.INDIC_SPELLING
+	if not interactive then buffer:indicator_clear_range(1, buffer.length) end
+	-- Iterate over spellcheckable text ranges, checking words in them, and marking misspellings.
+	local spellcheckable_styles = {} -- cache
+	local buffer, style_at = buffer, buffer.style_at
+	local encoding = M.spellchecker:get_dic_encoding()
+	local i = (not interactive or wrapped) and 1 or
+		buffer:word_start_position(buffer.current_pos, false)
+	while i <= buffer.length do
+		-- Ensure at least the next page of text is styled since spellcheckable ranges depend on
+		-- accurate styling.
+		if i > buffer.end_styled then
+			local next_page = buffer:line_from_position(i) + view.lines_on_screen
+			buffer:colorize(buffer.end_styled, buffer.line_end_position[next_page])
+		end
+		local style = style_at[i]
+		if spellcheckable_styles[style] == nil then
+			-- Update the cache.
+			local style_name = buffer:name_of_style(style)
+			spellcheckable_styles[style] = M.spellcheckable_styles[style_name] == true
+		end
+		if spellcheckable_styles[style] then
+			local j = i + 1
+			while j <= buffer.length and style_at[j] == style do j = j + 1 end
+			for e, s, word in lpeg_gmatch(word_patt, buffer:text_range(i, j)) do
+				local ok, encoded_word = pcall(string.iconv, word, encoding, 'UTF-8')
+				if not M.spellchecker:spell(ok and encoded_word or word) then
+					buffer:indicator_fill_range(i + s - 1, e - s)
+					if interactive then
+						buffer:goto_pos(i + s - 1)
+						show_suggestions(word)
+						return
+					end
+				end
+			end
+			i = j
+		else
+			i = i + 1
+		end
+	end
+	if interactive then
+		if not wrapped then
+			M.check_spelling(true, true) -- wrap
+			return
+		end
+		ui.statusbar_text = _L['No misspelled words.']
+	end
 end
 -- Check spelling upon saving files.
 events.connect(events.FILE_AFTER_SAVE,
-  function() if M.check_spelling_on_save then M.check_spelling() end end)
+	function() if M.check_spelling_on_save then M.check_spelling() end end)
 -- Show spelling suggestions when clicking on misspelled words.
 events.connect(events.INDICATOR_CLICK, function(position)
-  if buffer:indicator_all_on_for(position) & 1 << M.INDIC_SPELLING - 1 > 0 then
-    buffer:goto_pos(position)
-    M.check_spelling(true)
-  end
+	if buffer:indicator_all_on_for(position) & 1 << M.INDIC_SPELLING - 1 > 0 then
+		buffer:goto_pos(position)
+		M.check_spelling(true)
+	end
 end)
 
 -- Set up indicators, add a menu, and configure key bindings.
 events.connect(events.VIEW_NEW, function()
-  view.indic_style[M.INDIC_SPELLING] = not CURSES and view.INDIC_DIAGONAL or view.INDIC_STRAIGHTBOX
-  view.indic_fore[M.INDIC_SPELLING] = view.colors.red
+	view.indic_style[M.INDIC_SPELLING] = not CURSES and view.INDIC_DIAGONAL or view.INDIC_STRAIGHTBOX
+	view.indic_fore[M.INDIC_SPELLING] = view.colors.red
 end)
 
 -- Add menu entries and configure key bindings.
@@ -270,41 +270,41 @@ local m_tools = textadept.menu.menubar['Tools']
 local found_area
 local SEP = {''}
 for i = 1, #m_tools - 1 do
-  if not found_area and m_tools[i + 1].title == _L['Bookmarks'] then
-    found_area = true
-  elseif found_area then
-    local label = m_tools[i].title or m_tools[i][1]
-    if 'Spelling' < label:gsub('^_', '') or m_tools[i][1] == '' then
-      table.insert(m_tools, i, {
-        title = _L['Spelling'], --
-        {_L['Check Spelling...'], function() M.check_spelling(true) end},
-        {_L['Mark Misspelled Words'], M.check_spelling}, --
-        SEP, {
-          _L['Load Dictionary...'], function()
-            local dicts = {}
-            for _, path in ipairs(M.hunspell_paths) do
-              if not lfs.attributes(path, 'mode') then goto continue end
-              for name in lfs.dir(path) do
-                if not name:find('%.dic$') then goto continue end
-                dicts[#dicts + 1] = name:match('(.+)%.dic$')
-                ::continue::
-              end
-              ::continue::
-            end
-            local button
-            i = ui.dialogs.list{title = _L['Select Dictionary'], items = dicts}
-            if i then M.load(dicts[i]) end
-          end
-        }, SEP, {
-          _L['Open User Dictionary'], function()
-            if not lfs.attributes(user_dicts) then lfs.mkdir(user_dicts) end
-            io.open_file(user_dicts .. (not WIN32 and '/' or '\\') .. 'user.dic')
-          end
-        }
-      })
-      break
-    end
-  end
+	if not found_area and m_tools[i + 1].title == _L['Bookmarks'] then
+		found_area = true
+	elseif found_area then
+		local label = m_tools[i].title or m_tools[i][1]
+		if 'Spelling' < label:gsub('^_', '') or m_tools[i][1] == '' then
+			table.insert(m_tools, i, {
+				title = _L['Spelling'], --
+				{_L['Check Spelling...'], function() M.check_spelling(true) end},
+				{_L['Mark Misspelled Words'], M.check_spelling}, --
+				SEP, {
+					_L['Load Dictionary...'], function()
+						local dicts = {}
+						for _, path in ipairs(M.hunspell_paths) do
+							if not lfs.attributes(path, 'mode') then goto continue end
+							for name in lfs.dir(path) do
+								if not name:find('%.dic$') then goto continue end
+								dicts[#dicts + 1] = name:match('(.+)%.dic$')
+								::continue::
+							end
+							::continue::
+						end
+						local button
+						i = ui.dialogs.list{title = _L['Select Dictionary'], items = dicts}
+						if i then M.load(dicts[i]) end
+					end
+				}, SEP, {
+					_L['Open User Dictionary'], function()
+						if not lfs.attributes(user_dicts) then lfs.mkdir(user_dicts) end
+						io.open_file(user_dicts .. (not WIN32 and '/' or '\\') .. 'user.dic')
+					end
+				}
+			})
+			break
+		end
+	end
 end
 local mod = (WIN32 or LINUX) and 'ctrl' or OSX and 'cmd' or CURSES and 'meta'
 keys[mod .. '+:'] = m_tools[_L['Spelling']][_L['Check Spelling...']][2]
--- spell.cxx
+++ spell.cxx
@@ -10,55 +10,55 @@ extern "C" {
 
 /** spellchecker:add_dic() Lua function. */
 static int hs_add_dic(lua_State *L) {
-  auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
-  return (hs->add_dic(luaL_checkstring(L, 2), luaL_optstring(L, 3, NULL)), 0);
+	auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
+	return (hs->add_dic(luaL_checkstring(L, 2), luaL_optstring(L, 3, NULL)), 0);
 }
 
 /** spellchecker:spell() Lua function. */
 static int hs_spell(lua_State *L) {
-  auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
-  std::string word = luaL_checkstring(L, 2);
-  return (lua_pushboolean(L, hs->spell(word)), 1);
+	auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
+	std::string word = luaL_checkstring(L, 2);
+	return (lua_pushboolean(L, hs->spell(word)), 1);
 }
 
 /** spellchecker:suggest() Lua function. */
 static int hs_suggest(lua_State *L) {
-  auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
-  std::string word = luaL_checkstring(L, 2);
-  std::vector<std::string> suggestions = hs->suggest(word);
-  lua_createtable(L, suggestions.size(), 0);
-  for (size_t i = 0; i < suggestions.size(); i++)
-    lua_pushstring(L, suggestions[i].c_str()), lua_rawseti(L, -2, i + 1);
-  return 1;
+	auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
+	std::string word = luaL_checkstring(L, 2);
+	std::vector<std::string> suggestions = hs->suggest(word);
+	lua_createtable(L, suggestions.size(), 0);
+	for (size_t i = 0; i < suggestions.size(); i++)
+		lua_pushstring(L, suggestions[i].c_str()), lua_rawseti(L, -2, i + 1);
+	return 1;
 }
 
 /** spellchecker:get_dic_encoding() Lua function. */
 static int hs_get_dic_encoding(lua_State *L) {
-  auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
-  return (lua_pushstring(L, hs->get_dic_encoding()), 1);
+	auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
+	return (lua_pushstring(L, hs->get_dic_encoding()), 1);
 }
 
 /** spellchecker:add_word() Lua function. */
 static int hs_add(lua_State *L) {
-  auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
-  std::string word = luaL_checkstring(L, 2);
-  return (hs->add(word), 0);
+	auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
+	std::string word = luaL_checkstring(L, 2);
+	return (hs->add(word), 0);
 }
 
 /** spellchecker.__gc() metamethod. */
 static int hs_gc(lua_State *L) {
-  auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
-  return (hs->~Hunspell(), 0);
+	auto hs = reinterpret_cast<Hunspell *>(luaL_checkudata(L, 1, "ta_spell"));
+	return (hs->~Hunspell(), 0);
 }
 
 /** spell() Lua function. */
 static int spell(lua_State *L) {
-  const char *aff_path = luaL_checkstring(L, 1);
-  const char *dic_path = luaL_checkstring(L, 2);
-  const char *key = luaL_optstring(L, 3, NULL);
-  new (reinterpret_cast<Hunspell *>(lua_newuserdata(L, sizeof(Hunspell))))
-    Hunspell(aff_path, dic_path, key);
-  return (luaL_setmetatable(L, "ta_spell"), 1);
+	const char *aff_path = luaL_checkstring(L, 1);
+	const char *dic_path = luaL_checkstring(L, 2);
+	const char *key = luaL_optstring(L, 3, NULL);
+	new (reinterpret_cast<Hunspell *>(lua_newuserdata(L, sizeof(Hunspell))))
+		Hunspell(aff_path, dic_path, key);
+	return (luaL_setmetatable(L, "ta_spell"), 1);
 }
 
 extern "C" {
@@ -74,11 +74,11 @@ static const luaL_Reg lib[] = {
 // clang-format on
 
 int luaopen_spell(lua_State *L) {
-  if (luaL_newmetatable(L, "ta_spell")) {
-    luaL_newlib(L, lib), lua_setfield(L, -2, "__index");
-    lua_pushcfunction(L, hs_gc), lua_setfield(L, -2, "__gc");
-  }
-  return (lua_pushcfunction(L, spell), 1);
+	if (luaL_newmetatable(L, "ta_spell")) {
+		luaL_newlib(L, lib), lua_setfield(L, -2, "__index");
+		lua_pushcfunction(L, hs_gc), lua_setfield(L, -2, "__gc");
+	}
+	return (lua_pushcfunction(L, spell), 1);
 }
 
 // Platform-specific Lua library entry points.
