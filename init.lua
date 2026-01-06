c798c707cefdced24f1ca64b71a572aeb58c19fc
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2026-01-05 23:57:08 -0500

    Updated copyright year.

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- Copyright 2015-2025 Mitchell. See LICENSE.
+-- Copyright 2015-2026 Mitchell. See LICENSE.
 
 --- Spell checking for Textadept.
 -- Install this module by copying it into your *~/.textadept/modules/* directory or Textadept's
